cb8f5bc14ba6ed88ddc3467accf9ed71029074ea
Ken Manheimer <ken.manheimer@gmail.com>
2022-12-17 02:34:05 +0000

    Reenable non-GDPR blocks, enable experimental add button and working coopy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -17,11 +17,26 @@
 import '@plone/volto/config';
 
 export default function applyConfig(config) {
+  console.warn("frontend/src/config.js");
   config.settings = {
     ...config.settings,
     isMultilingual: false,
     supportedLanguages: ['en'],
     defaultLanguage: 'en',
   };
+
+  // Re-enable non-GDPR compliant blocks, inhibited by @eeacms/volto-eea-kitkat:
+  config.blocks.blocksConfig.video.restricted = false;
+  config.blocks.blocksConfig.maps.restricted = false;
+  config.blocks.blocksConfig.html.restricted = false;
+
+  // https://github.com/plone/volto/blob/621c5a727a0f3d9a03cbf58b64bd94c5d8834147/CHANGELOG.md#1600-alpha53-2022-11-18
+  // Experimental setting to move the button for adding a new block to show
+  // below any selected block, instead of only on the left of empty text
+  // blocks.
+  config.experimental.addBlockButton.enabled = true;
+
+  // https://6.dev-docs.plone.org/volto/configuration/workingcopy.html
+  config.settings.hasWorkingCopySupport = true;
   return config;
 }
