41c900ab37f6fb938cf3e15b215615ac82cd1ff0
Ken Manheimer <ken.manheimer@gmail.com>
2022-12-18 11:59:50 +0000

    Adjust mostUsed blocks list. Include blocks structure note.
    The blocks structure reveals details like the block ids. (The values are before the config.js adjustments.)

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -17,7 +17,7 @@
 import '@plone/volto/config';
 
 export default function applyConfig(config) {
-  console.warn("frontend/src/config.js");
+
   config.settings = {
     ...config.settings,
     isMultilingual: false,
@@ -29,6 +29,29 @@ export default function applyConfig(config) {
   config.blocks.blocksConfig.video.restricted = false;
   config.blocks.blocksConfig.maps.restricted = false;
   config.blocks.blocksConfig.html.restricted = false;
+
+  // add mostUsed:
+  [config.blocks.blocksConfig.text,
+   config.blocks.blocksConfig.slate,
+   config.blocks.blocksConfig.description,
+   config.blocks.blocksConfig.columnsBlock,
+   config.blocks.blocksConfig.accordion,
+   config.blocks.blocksConfig.columnsBlock,
+   config.blocks.blocksConfig.codeBlock,
+   config.blocks.blocksConfig.dividerBlock,
+  ].forEach(arg => { if (arg) { arg.mostUsed = true; }});
+
+  // remove mostUsed:
+  [config.blocks.blocksConfig.image,
+   config.blocks.blocksConfig.video,
+   config.blocks.blocksConfig.__grid,
+   config.blocks.blocksConfig.teaserGrid,
+   config.blocks.blocksConfig.imagesGrid,
+   config.blocks.blocksConfig.listing,
+   config.blocks.blocksConfig.teaser,
+   config.blocks.blocksConfig.carousel,
+   config.blocks.blocksConfig.slider,
+  ].forEach(arg => { if (arg) { arg.mostUsed = false; }});
 
   // https://github.com/plone/volto/blob/621c5a727a0f3d9a03cbf58b64bd94c5d8834147/CHANGELOG.md#1600-alpha53-2022-11-18
   // Experimental setting to move the button for adding a new block to show
