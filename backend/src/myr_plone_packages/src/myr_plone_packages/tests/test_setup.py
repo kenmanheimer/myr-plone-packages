2e4c3b5d86187b3ebbbbef88a297ba15dcf5e725
Ken Manheimer <ken.manheimer@gmail.com>
2022-12-18 12:25:41 +0000

    Try to settle lint-black issues.

--- backend/src/myr_plone_packages/src/myr_plone_packages/tests/test_setup.py
+++ backend/src/myr_plone_packages/src/myr_plone_packages/tests/test_setup.py
@@ -1,5 +1,7 @@
 """Setup tests for this package."""
-from myr_plone_packages.testing import MYR_PLONE_PACKAGES_INTEGRATION_TESTING  # noqa: E501
+from myr_plone_packages.testing import (
+    MYR_PLONE_PACKAGES_INTEGRATION_TESTING,
+)  # noqa: E501
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
